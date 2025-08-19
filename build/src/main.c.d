29bb06f2853d2b0fc2eea0e8cb5b7411e4a6ddb6
Aranykacsa <zalanvadasz@gmail.com>
2025-08-19 12:43:19 +0200

    i2c cleaning

--- build/src/main.c.d
+++ build/src/main.c.d
@@ -62,8 +62,7 @@ build/src/main.c.obj: src/main.c \
  /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/instance/usb.h \
  /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/instance/wdt.h \
  /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/pio/samd21g18a.h \
- /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/system_samd21.h \
- src/drivers/i2c/i2c.h src/drivers/uart/uart.h
+ /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/system_samd21.h
 /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/samd21.h:
 /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/samd21g18a.h:
 /home/vdsz_zalan/.cache/arm/packs/ARM/CMSIS/6.2.0/CMSIS/Core/Include/core_cm0plus.h:
@@ -128,5 +127,3 @@ build/src/main.c.obj: src/main.c \
 /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/instance/wdt.h:
 /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/pio/samd21g18a.h:
 /home/vdsz_zalan/.cache/arm/packs/Keil/SAMD21_DFP/1.3.1/Device/SAMD21A/Include/system_samd21.h:
-src/drivers/i2c/i2c.h:
-src/drivers/uart/uart.h:
