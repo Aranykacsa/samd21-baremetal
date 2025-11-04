4e95cab326cb9bb762224fdf9b4aa1c51decb0c9
Aranykacsa <zalanvadasz@gmail.com>
2025-11-04 22:40:15 +0100

    cleanup

--- src/main.c
+++ src/main.c
@@ -1,73 +1,29 @@
-/*#include "samd21.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <stddef.h>
+#include "clock.h"
+#include "samd21.h"
 #include "system_samd21.h"
 #include "uart.h"
-#include "clock.h"
-#include "i2c-master.h"
-#include "i2c-helper.h"
-#include "clock.h"
 #include "variables.h"
-#include "board.h"
-#include "storage.h"
-#include "radio.h"*/
+#include <stdint.h>
+#include <stdio.h>
 
-/*float t = 0, h = 0;
-char tx_cmd[64];
-
+// === Main ===
 int main(void) {
-  SystemInit();                               // 48 MHz clocks
+  SystemInit();
   SysTick_Config(SystemCoreClock / 1000);
+  uart_init(&uart_s2);
+  delay_ms(100);
 
-  setup_board();
-  setup_peripherals();
-
-  uint32_t next = get_uptime();
+  char buffer[10];
+  int index = 0;
+  uart_send_string(&uart_s2, "radio get mod\r\n");
 
   while (1) {
-    if ((int32_t)(get_uptime() - next) >= 0) {
-        next += 1000;
-        measure_env(&t, &h);
+    // uart_write(&uart_s2, buffer[index++]);
+    uint8_t c = uart_read(&uart_s2); // wait for a byte
+    buffer[index++] = c;
+    if (c == '\n')
+      printf("Received: %s\r\n", buffer);
 
-        snprintf(tx_cmd, sizeof(tx_cmd), "%.2fb%.2f0", (double)t, (double)h);
-        for (size_t i = 0; i < sizeof(tx_cmd); i++) {
-          if (tx_cmd[i] == '.') tx_cmd[i] = 'a';
-        }
-
-        printf("[RADIO] MSG: %s\r\n",tx_cmd);
-
-        // --- send it ---
-        radio_message(tx_cmd);
-    }
+    // delay_ms(100);
   }
-}*/
-
-    #include "samd21.h"
-    #include "system_samd21.h"
-    #include <stdint.h>
-    #include <stdio.h>
-    #include "uart.h"
-    #include "clock.h"
-#include "variables.h"
-
-    // === Main ===
-    int main(void) {
-        SystemInit();
-        SysTick_Config(SystemCoreClock / 1000);
-        uart_init(&uart_s2);
-        delay_ms(100);
-
-        char buffer[10];
-        int index = 0;
-        uart_send_string(&uart_s2, "radio get mod\r\n");
-
-        while (1) {
-            //uart_write(&uart_s2, buffer[index++]);
-            uint8_t c = uart_read(&uart_s2);   // wait for a byte
-            buffer[index++] = c;
-            if(c == '\n') printf("Received: %s\r\n", buffer);
-
-            //delay_ms(100);
-        }
-    }
+}
